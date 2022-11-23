8e7254ddabbea73b1cd90b0bf7deeaec100056ea
Corocottax <santicorocottax@gmail.com>
2022-11-23 19:19:59 +0100

    no required

--- src/api/users/users.model.js
+++ src/api/users/users.model.js
@@ -7,10 +7,10 @@ const userSchema = new Schema(
   {
     email: { type: String, required: true, unique: true, trim: true },
     password: { type: String, required: true, trim: true },
-    age: { type: Number, required: true },
+    age: { type: Number, required: false },
     rol: {type: String, enum: ["admin", "user"], default: "user"},
-    firstName: { type: String, required: true, trim: true },
-    lastName: { type: String, required: true, trim: true },
+    firstName: { type: String, required: false, trim: true },
+    lastName: { type: String, required: false, trim: true },
     characters: [{ type: mongoose.Schema.Types.ObjectId, trim: true, ref: 'characters' }],
     photo: { type: String, required: false, trim: true },
   },
